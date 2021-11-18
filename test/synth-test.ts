5c3f2f84744f91532539573fa414257673bea958
Logan Kearsley <chronosurfer@gmail.com>
2021-11-18 13:58:01 -0700

    Improve synth test

--- test/synth-test.ts
+++ test/synth-test.ts
@@ -1,44 +1,53 @@
+import 'mocha';
+import { expect } from 'chai';
 import fs from 'fs';
+import path from 'path';
 import WavEncoder from 'wav-encoder'; 
-import { synthesize } from "../src";
+import { CurveInput, synthesize } from "../src";
 
 const sampleRate = 44100;
+const segments: CurveInput = [{
+  f: {
+    type: 'transition',
+    sy: 440,
+    ey: 880,
+  },
+  a: {
+    type: 'contour',
+    y: 0.25,
+    a: 0.75,
+  },
+  run: 1000,
+}, {
+  f: {
+    type: 'transition',
+    curve: 'convex',
+    sy: 880,
+    ey: 220,
+  },
+  a: {
+    type: 'transition',
+    sy: 0.25,
+    ey: 0.75,
+  },
+  run: 1000,
+}];
 
-const [PCM ] = synthesize({
-  settings: { sampleRate },
-  segments: [{
-    f: {
-      type: 'transition',
-      sy: 440,
-      ey: 880,
-    },
-    a: {
-      type: 'contour',
-      y: 0.25,
-      a: 0.75,
-    },
-    run: 1000,
-  }, {
-    f: {
-      type: 'transition',
-      curve: 'convex',
-      sy: 880,
-      ey: 220,
-    },
-    a: {
-      type: 'transition',
-      sy: 0.25,
-      ey: 0.75,
-    },
-    run: 1000,
-  }],
-});
+describe("Curve Synthesis Test", function() {
+  this.timeout(0);
+  it("Should synthesize PCM data", () => {
+    const buffer = fs.readFileSync(path.join(__dirname, "fixtures/synth1.pcm"));
+    const a = new Uint8Array(buffer);
+    const [PCM] = synthesize({ segments,settings: { sampleRate } });
+    const b = new Uint8Array((PCM as Float32Array).buffer);
+    
+    WavEncoder.encode({
+      sampleRate,
+      channelData: [PCM as Float32Array],
+    }).then((buffer: ArrayBuffer) => {
+      fs.writeFileSync('synth1.wav', new Uint8Array(buffer));
+    });
 
-fs.writeFileSync('synth1.pcm', new Uint8Array((PCM as Float32Array).buffer));
-
-WavEncoder.encode({
-  sampleRate,
-  channelData: [PCM as Float32Array],
-}).then((buffer: ArrayBuffer) => {
-  fs.writeFileSync('synth1.wav', new Uint8Array(buffer));
+    expect(b).to.eql(a);
+  });
 });
